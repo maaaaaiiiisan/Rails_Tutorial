e350f5593a84844b6e8a46227c1c37ced9f5e73c
maaaaaiiiisan <mai.fukuzumi21@gmail.com>
2019-11-03 07:31:57 +0000

    Add account activation

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,6 +33,7 @@ Rails.application.configure do
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { host: '031d8a1fb4ac4dae9124331db0f050d5.vfs.cloud9.us-east-2.amazonaws.com' }
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
